4a07b2e12032e63d799d4879c018c529e732b8cd
Gergely POLONKAI (W00d5t0ck) <polesz@w00d5t0ck.info>
2012-09-03 23:38:10 +0200

    Added update.sh for easier code updating
    Signed-off-by: Gergely POLONKAI (W00d5t0ck) <gergely@polonkai.eu>

---
+++ update.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+git pull && \
+git submodule init && \
+git submodule update && \
+(app/console cache:clear --env=prod || rm -rf `dirname $0`/app/cache/prod) && \
+(app/console cache:clear || rm -rf `dirname $0`/app/cache/dev) && \
+app/console assets:install && \
+app/console assetic:dump --env=prod && \
+exit 0
+
+exit 1
+
