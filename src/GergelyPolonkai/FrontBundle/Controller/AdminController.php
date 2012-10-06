6a01dc5d975196f595c601bf27b33205e65f6364
Gergely POLONKAI <polesz@w00d5t0ck.info>
2012-10-06 13:22:24 +0200

    Renamed newBlogPost to editBlogPost

--- src/GergelyPolonkai/FrontBundle/Controller/AdminController.php
+++ src/GergelyPolonkai/FrontBundle/Controller/AdminController.php
@@ -55,7 +55,7 @@ class AdminController extends Controller
      * @Route("/blog/post/{id}", name="GergelyPolonkaiFrontBundle_adminEditBlogPost", defaults={"id": null})
      * @Template
      */
-    public function newBlogPostAction($id = null)
+    public function editBlogPostAction($id = null)
     {
         if (is_numeric($id)) {
             $post = $this->getDoctrine()->getRepository('GergelyPolonkaiFrontBundle:Post')->findOneById($id);
